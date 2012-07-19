78acd451a00aa3d163d93a38082464790097eda0
Zack Wyatt <Zack@Zacks-MacBook-Pro.local>
2012-07-18 23:22:06 -0600

    Added changes

--- Wyatt_Zack_Assignment3.js
+++ Wyatt_Zack_Assignment3.js
@@ -4,3 +4,28 @@
 //Week 3
 
 //Global Variables
+var flight= "Southwest";
+var amntOfPassenger= "47";
+var destination= ["Boise", "Las Vegas", "Denver"];
+var announce= "Ready to board Flight ";
+
+//Object
+
+    
+    
+//Procedure
+
+//For Loop
+
+
+
+//Boolean
+
+
+//Mutator
+
+
+//Accessor
+
+//While Loop
+    
