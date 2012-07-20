2951c47ee36b5d65da8bbf2ea10fa746e3a996d6
Zack Wyatt <Zack@Zacks-MacBook-Pro.local>
2012-07-19 21:58:09 -0600

    Upadted files.

--- Wyatt_Zack_Assignment3.js
+++ Wyatt_Zack_Assignment3.js
@@ -1,2 +1,46 @@
 //alert("JavaScript works!");
-console.log;("And the crowd goes wild!");
+
+// Global Variables
+var team1= "The Flame";
+var team2= "The Lightening";
+var series= "7";
+var event= "ABC Finals";
+var say = function(message) { console.log(message); };
+
+//Object
+var game = {
+    teams: [team1, team2],
+    Lightening:"99",
+    whosUp: function() {
+        console.log(team2 +" is up by 1");
+    },
+    secondsLeft: "10",
+    
+};
+
+//Procedure
+console.log("Welcome to game " + series + "!");
+console.log(team1 + " vs " + team2);
+game.whosUp();
+
+//For Loop
+
+console.log("Starting for " + team1);
+var listOfPlayers = function (json) {
+	for (var i = 0; i < json.players.length; i++) {
+		var player = json.players[i];
+		say("Name: " + player.name + ", Position: " + player.position + ", Height: " + player.height);
+	};
+};
+
+listOfPlayers(json);
+
+console.log("Starting for " + team2);
+var listOfOpponents = function (json2) {
+	for (var i = 0; i < json2.opponents.length; i++) {
+		var player = json2.opponents[i];
+		say("Name: " + player.name + ", Position: " + player.position + ", Height: " + player.height);
+
+        };
+};
+listOfOpponents(json2);
---
+++ json.js
@@ -0,0 +1,47 @@
+//Zack Wyatt
+//SDI 1207
+//Project 3
+
+var json = {
+	"players": [
+		{
+			"position": "PF",
+			"name": "Lebron",
+			"height" : "6'8\""
+		},
+		{
+			"position": "PG",
+			"name": "Koby",
+			"height" : "6'6\""
+		},
+		{
+			"position": "SG",
+			"name": "Michael",
+			"height" : "6'6\""
+		}
+	]
+};
+
+var json2 = {
+        "opponents": [
+            
+            {
+			"position": "PF",
+			"name": "Kevin",
+			"height" : "6'8\""
+		},
+		{
+			"position": "PG",
+			"name": "Russell",
+			"height" : "6'6\""
+		},
+		{
+			"position": "SG",
+			"name": "James",
+			"height" : "6'6\""
+		}
+            
+            ]
+    
+    
+    };
